71f8098530ec93937a326f4f549f881f48343396
URK21CS2023 JOHN BLESSINGH J S <101979951+JohnBlessingh@users.noreply.github.com>
2022-08-10 10:55:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,29 +4,29 @@ class Evaluate:
     self.size_of_stack = size
     self.stack = [ ]
  def isEmpty(self):
-    if self.top == -1:
-      return True
-    else:
-      return False
+  if self.top == -1:
+    return True
+  else:
+    return False
  def pop(self):
-    if not self.isEmpty():
+  if not self.isEmpty():
     self.stack.pop()
  
 def push(self, operand):
-    if self.top != self.size_of_stack - 1:
+  if self.top != self.size_of_stack - 1:
     self.stack.append(operand)
 def validate_postfix_expression(self, expression):
-    a = 0
-    b = 0
-    for element in expression:
-      if element.isnumeric():
-        a = a + 1
-      else:
-        b = b + 1
- if b == a - 1:
-  return True
- else:
- return False
+  a = 0
+  b = 0
+  for element in expression:
+    if element.isnumeric():
+      a = a + 1
+    else:
+      b = b + 1
+  if b == a - 1:
+    return True
+  else:
+    return False
  
 def evaluate_postfix_expression(self, expression):
    stack = []
